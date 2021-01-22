60b8f8caa36346b76d0d9aa5a36ff31df2dace10
domire8 <dominic.reber@epfl.ch>
2021-01-22 12:58:26 +0100

    fix path of franka lwi header file

--- control/scripts/setup.sh
+++ control/scripts/setup.sh
@@ -14,5 +14,5 @@ fi
   mkdir -p ../include/franka_lwi && \
   cd ../include/franka_lwi && \
   curl -o franka_lwi_communication_protocol.h \
-  "https://raw.githubusercontent.com/epfl-lasa/franka_lightweight_interface/main/include/franka_lightweight_interface/franka_lwi_communication_protocol.h?token=AHXSTB437OAZQKPIAJMHSE3ABBCMM"
+  "https://raw.githubusercontent.com/epfl-lasa/franka_lightweight_interface/main/include/franka_lightweight_interface/franka_lwi_communication_protocol.h?token=AQ7UUDR2L77SWNGGJSCMDQ3ACP37W"
 )

ee9fa69f28a65a4aba06948c087f6db4cb62a962
domire8 <71256590+domire8@users.noreply.github.com>
2021-02-03 17:23:11 +0100

    Update Dockerfile with control_libraries (#23)
    * Use control_libraries instead of modulo, install libsodium/zmq binaries directly
    
    * Add the --no-cache option

--- control/scripts/build.sh
+++ control/scripts/build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 # Builds the runtime image (compiled applications)
+REBUILD=0
+while getopts 'r' opt; do
+  case $opt in
+  r) REBUILD=1 ;;
+  *)
+    echo 'Error in command line parsing' >&2
+    exit 1
+    ;;
+  esac
+done
+shift "$((OPTIND - 1))"
 
 path=$(echo "$PWD" | rev | cut -d'/' -f-3 | rev)
 if [ "$path" != "CITRIFIED/control/scripts" ]; then
@@ -10,4 +21,8 @@ fi
 
 IMAGE_NAME=citrified/control/runtime
 
-docker build --target runtime -f ../Dockerfile --tag $IMAGE_NAME ..
+if [ "$REBUILD" -eq 1 ]; then
+  docker build --no-cache --target runtime -f ../Dockerfile --tag $IMAGE_NAME ..
+else
+  docker build --target runtime -f ../Dockerfile --tag $IMAGE_NAME ..
+fi

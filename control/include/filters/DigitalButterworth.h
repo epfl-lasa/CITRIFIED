574ec4789964c89c0c307741d4c73a02fbac1252
domire8 <dominic.reber@epfl.ch>
2021-03-09 08:27:43 +0100

    Filter has an i/o dimension now and handles Eigen vectors

--- control/include/filters/DigitalButterworth.h
+++ control/include/filters/DigitalButterworth.h
@@ -2,22 +2,28 @@
 
 #include <string>
 #include <vector>
+#include <eigen3/Eigen/Core>
 
 namespace filter {
 
 class DigitalButterworth {
 public:
-  explicit DigitalButterworth(const std::string& filterName, const std::string& filePath);
+  explicit DigitalButterworth(const std::string& filterName, const std::string& filePath, const int& dimensions);
 
   double computeFilterOutput(const double& input);
+
+  Eigen::VectorXd computeFilterOutput(const Eigen::VectorXd& input);
 
   void resetFilter();
 
 private:
-  double order_;
+  double differenceEquations(const double& input, const int& wIndex);
+
+  const int dim_;
+  int order_;
   std::vector<double> numeratorCoeffs_;
   std::vector<double> denominatorCoeffs_;
-  std::vector<double> w_;
+  std::vector<Eigen::VectorXd> w_;
 };
 
 }
--- control/source/filters/DigitalButterworth.cpp
+++ control/source/filters/DigitalButterworth.cpp
@@ -5,9 +5,10 @@
 
 namespace filter {
 
-DigitalButterworth::DigitalButterworth(const std::string& filterName, const std::string& filePath) {
+DigitalButterworth::DigitalButterworth(const std::string& filterName, const std::string& filePath,
+                                       const int& dimensions) : dim_(dimensions) {
   YAML::Node params = YAML::LoadFile(filePath);
-  order_ = params[filterName]["order"].as<double>();
+  order_ = params[filterName]["order"].as<int>();
   auto numCoeffs = params[filterName]["numerator_coefficients"].as<std::vector<double>>();
   auto denomCoeffs = params[filterName]["denominator_coefficients"].as<std::vector<double>>();
   if (numCoeffs.size() != order_ + 1 || denomCoeffs.size() != order_ + 1) {
@@ -16,21 +17,44 @@ DigitalButterworth::DigitalButterworth(const std::string& filterName, const std:
   }
   numeratorCoeffs_ = numCoeffs;
   denominatorCoeffs_ = denomCoeffs;
-  w_.resize(order_);
+  w_.resize(dim_, Eigen::VectorXd::Zero(order_));
   resetFilter();
 }
 
 double DigitalButterworth::computeFilterOutput(const double& input) {
-  double output = numeratorCoeffs_.at(0) * input + w_.at(0);
-  for (size_t i = 0; i < order_ - 1; ++i) {
-    w_.at(i) = numeratorCoeffs_.at(i + 1) * input + w_.at(i + 1) - denominatorCoeffs_.at(i + 1) * output;
+  if (dim_ == 1) {
+    return differenceEquations(input, 0);
+  } else {
+    throw std::invalid_argument(
+        "This filter has input dimension " + std::to_string(dim_) + ". Check the size of your input");
   }
-  w_.back() = numeratorCoeffs_.back() * input - denominatorCoeffs_.back() * output;
+}
+
+Eigen::VectorXd DigitalButterworth::computeFilterOutput(const Eigen::VectorXd& input) {
+  if (dim_ == input.size()) {
+    Eigen::VectorXd output(dim_);
+    for (std::size_t dim = 0; dim < dim_; ++dim) {
+      output(dim) = differenceEquations(input(dim), dim);
+    }
+    return output;
+  } else {
+    throw std::invalid_argument(
+        "This filter has input dimension " + std::to_string(dim_) + ". Check the size of your input");
+  }
+}
+
+double DigitalButterworth::differenceEquations(const double& input, const int& wIndex) {
+  double output = numeratorCoeffs_.at(0) * input + w_.at(wIndex)(0);
+  for (size_t i = 0; i < order_ - 1; ++i) {
+    w_.at(wIndex)(i) =
+        numeratorCoeffs_.at(i + 1) * input + w_.at(wIndex)(i + 1) - denominatorCoeffs_.at(i + 1) * output;
+  }
+  w_.at(wIndex)(order_ - 1) = numeratorCoeffs_.back() * input - denominatorCoeffs_.back() * output;
   return output;
 }
 
 void DigitalButterworth::resetFilter() {
-  std::fill(w_.begin(), w_.end(), 0);
+  std::fill(w_.begin(), w_.end(), Eigen::VectorXd::Zero(order_));
 }
 
 }

cc260d073dec67e8187b851f55c768bb3a3980ca
Enrico Eberhard <enrico.eberhard@gmail.com>
2021-03-18 18:03:49 +0100

    Support state_rep in CartPoseCtrl, revise point attractor
    * Add methods to use exclusively state_representation types
    in CartesianPoseController
    
    * Rename simple_ds_controller -> simple_point_attractor
    
    * Revise point attractor demo to use control libraries
    components more explicitly

--- control/include/controllers/CartesianPoseController.h
+++ control/include/controllers/CartesianPoseController.h
@@ -3,24 +3,34 @@
 
 #include <franka_lwi/franka_lwi_communication_protocol.h>
 #include <passive-ds-control/passive_ds_controller.h>
+#include <state_representation/space/cartesian/CartesianState.hpp>
+#include <state_representation/space/cartesian/CartesianTwist.hpp>
+#include <state_representation/space/cartesian/CartesianWrench.hpp>
+#include <state_representation/robot/JointTorques.hpp>
+#include <state_representation/robot/Jacobian.hpp>
 
 namespace controller {
 
 class CartesianLinearSpaceController {
 public:
-  CartesianLinearSpaceController();
-  CartesianLinearSpaceController(double d0, double d1);
+  explicit CartesianLinearSpaceController(double d0 = 50, double d1 = 50);
   void setDamping(double d0, double d1);
+
   frankalwi::proto::CommandMessage<7> getJointTorque(frankalwi::proto::StateMessage<7> state,
                                                      const std::vector<double>& twist);
+
   static frankalwi::proto::CommandMessage<7> getJointTorque(frankalwi::proto::StateMessage<7> state,
                                                             const Eigen::Matrix<double, 6, 1>& wrench);
+
+  virtual state_representation::CartesianWrench getWrenchCommand(const state_representation::CartesianTwist& state,
+                                                                 const state_representation::CartesianTwist& command);
 protected:
   virtual Eigen::Matrix<double, 6, 1> getWrenchCommand(frankalwi::proto::StateMessage<7> state,
                                                        const std::vector<double>& twist);
+
   PassiveDSController* controller_;
-  double d0_ = 50;
-  double d1_ = 50;
+  double d0_;
+  double d1_;
   double maxTankLevel_ = 100;
   double dz_ = 0.01;
   double maxForce = 50;
@@ -28,15 +38,15 @@ protected:
 
 class CartesianAngularSpaceController : public CartesianLinearSpaceController {
 public:
-  CartesianAngularSpaceController() = default;
-  explicit CartesianAngularSpaceController(double k);
-  CartesianAngularSpaceController(double k, double d);
+  explicit CartesianAngularSpaceController(double k = 1, double d = 1);
   void setStiffness(double k);
   void setDamping(double d);
 
+  state_representation::CartesianWrench getWrenchCommand(const state_representation::CartesianTwist& state,
+                                                         const state_representation::CartesianTwist& command) override;
 protected:
-  double k_ = 25;
-  double d_ = 0;
+  double k_;
+  double d_;
   double maxTorque = 50;
   Eigen::Matrix<double, 6, 1> getWrenchCommand(frankalwi::proto::StateMessage<7> state,
                                                const std::vector<double>& twist) override;
@@ -44,12 +54,16 @@ protected:
 
 class CartesianPoseController {
 public:
-  CartesianPoseController(double linearD0, double linearD1, double angularK);
+  explicit CartesianPoseController(double linearD0 = 1, double linearD1 = 1, double angularK = 1, double angularD = 0);
 
   CartesianLinearSpaceController linearController;
   CartesianAngularSpaceController angularController;
   frankalwi::proto::CommandMessage<7> getJointTorque(frankalwi::proto::StateMessage<7> state,
                                                      const std::vector<double>& twist);
+
+  state_representation::JointTorques getJointTorque(const state_representation::CartesianTwist& state,
+                                                    const state_representation::CartesianTwist& command,
+                                                    const state_representation::Jacobian& jacobian);
 };
 
 }

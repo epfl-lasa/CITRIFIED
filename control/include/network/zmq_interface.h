8034e7a61179f3025fd94105d2983d656897ca9f
domire8 <dominic.reber@epfl.ch>
2021-04-19 16:44:53 +0200

    implement PAIR communication

--- control/include/network/zmq_interface.h
+++ control/include/network/zmq_interface.h
@@ -4,12 +4,12 @@
 
 namespace network::zmq_interface {
 
-inline void configureSockets(zmq::context_t& context,
-                             zmq::socket_t& publisher,
-                             zmq::socket_t& subscriber,
-                             const std::string& subscriber_uri,
-                             const std::string& publisher_uri,
-                             const bool bind = true) {
+inline void configurePubSubSockets(zmq::context_t& context,
+                                   zmq::socket_t& publisher,
+                                   zmq::socket_t& subscriber,
+                                   const std::string& subscriber_uri,
+                                   const std::string& publisher_uri,
+                                   const bool bind = true) {
   context = zmq::context_t(1);
 
   subscriber = zmq::socket_t(context, ZMQ_SUB);
@@ -27,20 +27,40 @@ inline void configureSockets(zmq::context_t& context,
   }
 }
 
+inline void configurePairSocket(zmq::context_t& context,
+                                zmq::socket_t& socket,
+                                const std::string& socket_uri,
+                                const bool bind = true) {
+  context = zmq::context_t(1);
+
+  socket = zmq::socket_t(context, ZMQ_PAIR);
+
+  if (bind) {
+    socket.bind("tcp://" + socket_uri);
+  } else {
+    socket.connect("tcp://" + socket_uri);
+  }
+}
+
 template<typename T>
-inline bool send(zmq::socket_t& publisher, const T& obj) {
+inline bool send(zmq::socket_t& socket, const T& obj) {
   zmq::message_t message(sizeof(obj));
   memcpy(message.data(), &obj, sizeof(obj));
-  auto res = publisher.send(message, zmq::send_flags::none);
+  auto res = socket.send(message, zmq::send_flags::none);
+  return res.has_value();
+}
 
+inline bool send_string(zmq::socket_t& socket, const std::string& obj) {
+  zmq::message_t message(obj.size());
+  memcpy(message.data(), obj.data(), obj.size());
+  auto res = socket.send(message, zmq::send_flags::none);
   return res.has_value();
 }
 
 template<typename T>
-inline bool receive(zmq::socket_t& subscriber, T& obj,
-             const zmq::recv_flags flags = zmq::recv_flags::none) {
+inline bool receive(zmq::socket_t& socket, T& obj, const zmq::recv_flags flags = zmq::recv_flags::none) {
   zmq::message_t message;
-  auto res = subscriber.recv(message, flags);
+  auto res = socket.recv(message, flags);
   if (res) {
     obj = *message.data<T>();
   }
@@ -48,8 +68,8 @@ inline bool receive(zmq::socket_t& subscriber, T& obj,
 }
 
 template<typename T>
-inline bool poll(zmq::socket_t& subscriber, T& obj) {
-  return receive(subscriber, obj, zmq::recv_flags::dontwait);
+inline bool poll(zmq::socket_t& socket, T& obj) {
+  return receive(socket, obj, zmq::recv_flags::dontwait);
 }
 
 }

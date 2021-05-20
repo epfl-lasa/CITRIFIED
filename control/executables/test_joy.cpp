a7e8d6695f676cd3696133b1dc9df56612697e17
domire8 <dominic.reber@epfl.ch>
2021-05-20 10:57:41 +0200

    minor fixes

--- control/executables/test_joy.cpp
+++ control/executables/test_joy.cpp
@@ -1,43 +1,18 @@
 #include <iostream>
-#include <cstdio>
+#include <unistd.h>
+#include <state_representation/space/cartesian/CartesianPose.hpp>
 
-#include <network/zmq_joy_protocol.h>
-
-#include "network/interfaces.h"
 #include "sensors/Joy.h"
 
-void throttledPrintState(int skip, double avg_freq) {
-  static int count = 0;
-  if (count > skip) {
-    printf("Average frequency of state messages: % 3.3f\n", avg_freq);
-    count = 0;
-  }
-  ++count;
-}
-
 int main(int argc, char** argv) {
-
-//  // Set up franka ZMQ
-//  network::Interface joy(network::InterfaceType::JOY);
-//  pure_joy::proto::JoyMessage joyMessage{};
-//
-//  auto start = std::chrono::system_clock::now();
-//  int iterations;
-//  bool received = false;
-//  while (joy.receive(joyMessage)) {
-//    if (!received) {
-//      received = true;
-//      start = std::chrono::system_clock::now();
-//      iterations = 0;
-//    }
-//    std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now() - start;
-////    std::cout << joyMessage.axes[0] << std::endl;
-//    throttledPrintState(500, iterations / elapsed_seconds.count());
-//    ++iterations;
-//  }
   sensors::Joy joy;
   joy.start();
+  auto pose = state_representation::CartesianPose::Identity("joy", "world");
   while (true) {
-    std::cout << joy.getJoyUpdate() << std::endl;
+    auto joyPose = state_representation::CartesianPose::Identity("joy", "world");
+    joy.getJoyUpdate(joyPose);
+    pose += joyPose;
+    std::cout << pose << std::endl;
+    usleep(1e6);
   }
 }

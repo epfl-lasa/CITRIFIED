3d9c80f7eb17acf9c23f3db29efef3ae3a71bcac
domire8 <71256590+domire8@users.noreply.github.com>
2021-06-08 13:14:10 +0200

    Use pubkey authentication for remote dev (#84)

--- control/scripts/remote-dev.sh
+++ control/scripts/remote-dev.sh
@@ -1,18 +1,12 @@
-#!/bin/sh
-# Builds and runs the remote development container
-# If this fails, try running the following command:
-# sudo ssh-keygen -f "$HOME/.ssh/known_hosts" -R 127.0.0.1:"$PORT"
-REBUILD=0
-while getopts 'r' opt; do
-  case $opt in
-  r) REBUILD=1 ;;
-  *)
-    echo 'Error in command line parsing' >&2
-    exit 1
-    ;;
-  esac
-done
-shift "$((OPTIND - 1))"
+#!/usr/bin/env bash
+
+IMAGE_NAME=citrified/control/remote-dev
+STAGE_NAME=remote-development
+CONTAINER_NAME=citrified-control-remote-development-ssh
+CONTAINER_HOSTNAME=citrified-control-remote-development
+
+SSH_PORT=2222
+SSH_KEY_FILE="$HOME/.ssh/id_rsa.pub"
 
 path=$(echo "$PWD" | rev | cut -d'/' -f-3 | rev)
 if [ "$path" != "CITRIFIED/control/scripts" ]; then
@@ -21,18 +15,45 @@ if [ "$path" != "CITRIFIED/control/scripts" ]; then
   exit 1
 fi
 
-IMAGE_NAME=citrified/control/remote-dev
-CONTAINER_NAME=citrified-control-remote-dev
-PORT_SSH=2222
+HELP_MESSAGE="Usage: ./remote-dev.sh [--port <port>] [--key-file </path/to/id_rsa.pub>] [-r]
+Build and run a docker container as an SSH toolchain server for remote development.
+The server is bound to the specified port on localhost (127.0.0.1)
+and uses passwordless RSA key-pair authentication. The host public key
+is read from the specified key file and copied to the server on startup.
+The server will run in the background as ${CONTAINER_NAME}.
+You can connect with 'ssh remote@localhost -p <port>'.
+Close the server with 'docker container stop ${CONTAINER_NAME}'.
+Options:
+  -p, --port [XXXX]        Specify the port to bind for SSH
+                           connection.
+                           (default: ${SSH_PORT})
+  -k, --key-file [path]    Specify the path of the RSA
+                           public key file.
+                           (default: ${SSH_KEY_FILE})
+  -r                       Rebuild the image with the '--no-cache' option
+  -h, --help               Show this help message."
+
+
+REBUILD=0
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -p|--port) SSH_PORT=$2; shift 1;;
+    -k|--key-file) SSH_KEY_FILE=$2; shift 1;;
+    -r) REBUILD=1; shift 1;;
+    -h|--help) echo "${HELP_MESSAGE}"; exit 0;;
+    -*) echo "Unknown option: $1" >&2; echo "${HELP_MESSAGE}"; exit 1;;
+  esac
+done
+
 PORT_OPTITRACK=5511
 PORT_GPR=7777
 PORT_JOY=8888
 PORT_BRIDGE=9999
 
 if [ "$REBUILD" -eq 1 ]; then
-  DOCKER_BUILDKIT=1 docker build --no-cache --target project-dependencies -f ../Dockerfile --tag $IMAGE_NAME ..
+  DOCKER_BUILDKIT=1 docker build --no-cache --target "${STAGE_NAME}" -f ../Dockerfile --tag "$IMAGE_NAME" ..
 else
-  DOCKER_BUILDKIT=1 docker build --target project-dependencies -f ../Dockerfile --tag $IMAGE_NAME ..
+  DOCKER_BUILDKIT=1 docker build --target "${STAGE_NAME}" -f ../Dockerfile --tag "$IMAGE_NAME" ..
 fi
 
 docker container stop "$CONTAINER_NAME" >/dev/null 2>&1
@@ -42,12 +63,14 @@ docker network inspect citrinet >/dev/null 2>&1 || \
   docker network create --subnet=172.20.0.0/16 --gateway=172.20.0.1 citrinet
 
 docker run -d --cap-add sys_ptrace \
+  --publish 127.0.0.1:"${SSH_PORT}":22 \
   --network=citrinet \
-  -p127.0.0.1:"$PORT_SSH":22 \
   -p1601:1601 -p1602:1602 \
   -p1701:1701 -p1702:1702 \
   -p"$PORT_OPTITRACK":"$PORT_OPTITRACK" \
   -p"$PORT_GPR":"$PORT_GPR" \
   -p"$PORT_JOY":"$PORT_JOY" \
   -p"$PORT_BRIDGE":"$PORT_BRIDGE" \
-  --name $CONTAINER_NAME $IMAGE_NAME
+  --name "${CONTAINER_NAME}" \
+  --hostname "${CONTAINER_HOSTNAME}" \
+  "${IMAGE_NAME}" "$(cat ${SSH_KEY_FILE})"
